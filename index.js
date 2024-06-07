168e51daf8d234fbe5547c82deae2d2ea92a1649
eclubit <eclubit2024@gmail.com>
2024-06-07 12:53:56 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
- 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+var nodemailer = require('nodemailer');
+
 app.get('/', (req, res) => {
   res
     .status(200)
     .send('Hello server is running')
+    .end();
+});
+
+app.get('/mail', (req, res) => {
+  res
+    .status(200)
+    .send('Sending Email...')
     .end();
 });
  
