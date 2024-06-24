5f17d907eaafd39e266abc72468113b2942c4cd3
eclubit <eclubit2024@gmail.com>
2024-06-24 10:06:37 +0530

    emailing added

--- index.js
+++ index.js
@@ -61,11 +61,11 @@ app.get('/my-courses', async(req, res) => {
 	res.sendFile("public/index.html", { root: __dirname })
 });
 
-app.post('/send-email', async(req, res) => {
-	var receiver = req.body.receiver;
-	var subject = req.body.subject;
-	var text = req.body.text;
-	var html = req.body.html;
+app.get('/send-email', async(req, res) => {
+	var receiver = "prageethpramuditha.97@gmail.com"//req.body.receiver;
+	var subject = "qwefqef"; //req.body.subject;
+	var text = "";
+	//var html = req.body.html;
 	const transporter = nodemailer.createTransport({
 	  service: "Gmail",
 	  host: "smtp.gmail.com",
@@ -80,8 +80,6 @@ app.post('/send-email', async(req, res) => {
 	var mailOptions = {
 	  from: 'office.eclub@gmail.com',
 	  to: receiver,
-	  //subject: 'Virtual Loyality Card Infromation',
-	  //html: await readFile("public/test.html", 'utf8')
 	  subject: subject,
 	  text: text,
 	  html: '<!doctype html><html lang="en"> <head></head> <body> <img style="height : 20%; width : 30%;" src="cid:eclub@logo"></div><p style="font-family : Comic Sans; margin-top : 4%;">Your account was created. Please login to <a href="https://office.eclub.lk/">https://office.eclub.lk/</a> using following credintials.</p><table style="border-collapse: collapse;border: 1px solid;"><tbody><tr style="border: 1px solid;"><td style="border: 1px solid; width : 20%; padding : 1%">Username</td><td style="border: 1px solid; width : 40%; padding : 1%"><h4>CCO365643</h4></td></tr><tr><td style="border: 1px solid; width : 20%; padding : 1%">Password</td><td style="border: 1px solid; width : 40%; padding : 1%"><h4>cR%g5fyrt@34</h4></td></tr></tbody></table><p style="margin-top : 4%; color : red;"><b>The above details are highly private and confidential so please avoid sharing them.</b></p> </body></html>',
