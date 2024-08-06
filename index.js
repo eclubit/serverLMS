3765d1e4e5ee83d57a0a5e580a7577d03c514d17
eclubit <eclubit2024@gmail.com>
2024-08-06 10:42:26 +0530

    all solved

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const path = require('path');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static('public'));
 
 var nodemailer = require('nodemailer');
 app.use((req, res, next) => {
@@ -18,7 +19,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/', (req, res) => {
+
+app.get('/test', (req, res) => {
+   res.send("Is Work Success");
+})
+
+app.post('/api/auth', (req, res) => {
 	let sr_no = req.body.key;
 	let data = new FormData();
 	data.append('user_id', sr_no);
@@ -65,68 +71,6 @@ app.post('/', (req, res) => {
 
 });
 
-app.get('/testing', (req, res) => {
-	let sr_no = "292440";//req.body.key;
-	let data = new FormData();
-	data.append('user_id', sr_no);
-	let config = {
-	  method: 'post',
-	  maxBodyLength: Infinity,
-	  url: 'https://www.eclub.lk/api/user_courses/all',
-	  headers: { 
-		'access_token': '$2y$10$S3W2WSR9sk5cf0yEqR1Rf.oZGkvhUu46idzux2QLMTTzt3m2IHIWS', 
-		'Content-Type': 'application/json', 
-		'Cookie': 'wtk_s=e8kvcekdr96vvqgmmectba1o23', 
-		...data.getHeaders()
-	  },
-	  data : data
-	};
-
-	axios.request(config)
-	.then((response) => {
-	  var resposeAll = response.data;
-	  var result = [];
-
-		for(var i in resposeAll){
-			var record1 = resposeAll[i].category + "- H";
-			var record2 = resposeAll[i].category + "- F";
-			if(i > 0){
-				for(var j = 0 ; j < result.length; j++){
-					if(result[j] == record1 || result[j] == record2){
-						
-					}
-					else {
-						result.push(resposeAll[i].category + "- " + resposeAll[i].status);
-					}
-				}
-			}
-			else {
-				result.push(resposeAll[i].category + "- " + resposeAll[i].status);
-			}
-		}
-		res.send(result);
-	})
-	.catch((error) => {
-	  console.log(error);
-	});
-
-});
-
-app.get('/auth/:id', async(req, res) => {
-	var id = req.params.id;
-	res.send('<!DOCTYPE html>' +
-			'	<head>' +
-			'	</head>' +
-			'	<body>' +
-			'		<script>localStorage.setItem("LoggedIn_SRNo","'+ id  +'");' +
-			'				 window.location.replace("/my-courses");</script>'+
-			'	</body>' +
-			'	</html>');
-});
-
-app.get('/my-courses', async(req, res) => {
-	res.sendFile("public/index.html", { root: __dirname })
-});
 
 app.post('/send-email', async(req, res) => {
 	let to = req.body.to;
@@ -170,15 +114,6 @@ app.post('/send-email', async(req, res) => {
 	})
 });
 
-
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('<!DOCTYPE html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Bootstrap demo</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><link rel="stylesheet" href="./css/style.css"></head><body><div class="w-50 mx-auto my-5"><div class="form-group"><label for="exampleInputEmail1">Enter Your SR Number</label><input type="text" class="form-control" id="data"></div><button type="submit" onclick="clicked()" class="btn btn-success my-2">Go LMS</button></div><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.3.0/mdb.umd.min.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script><script>function clicked(){var url="/auth/" + document.getElementById("data").value;window.location.href=url;}</script></body></html>')
-    .end();
-});
-
- 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
